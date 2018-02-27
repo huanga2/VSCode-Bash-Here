3fab803060d1bf7dea072acf98b52bd069d0a2e5
Andrew <huanga2@student.unimelb.edu.au>
2018-02-28 01:35:20 +1100

    complete

--- src/extension.ts
+++ src/extension.ts
@@ -9,16 +9,33 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-bash-context" is now active!');
+    console.log('vscode-bash-context is active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.BashHere', (selectedItem : vscode.Uri) => {
         // The code you place here will be executed every time your command is executed
 
+        
+        if (selectedItem == undefined) {
+            return;
+        }
+        
+        var onlyPath = require('path').dirname(selectedItem.fsPath);
+
+        // console.log(onlyPath);
+
+        const{spawn} = require("child_process");
+
+        var WSLPath = vscode.workspace.getConfiguration("BashHere").WSLPath;
+        // console.log(WSLPath);
+
+
+        spawn(WSLPath, ['-c', 'bash'], {shell: true, cwd: onlyPath, detached: true});
+
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
